a2977848ac9a375f334c433389e7973e42c53f53
Caioff0123 <cf153317@gmail.com>
2025-12-07 10:59:26 -0300

    Corrige Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+for i in range(1, 11):
+    print(i)
